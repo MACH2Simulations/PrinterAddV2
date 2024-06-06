1ed4b9e697fe3431f47cbddeea94357f19d6780c
MACH2Simulations <9164503+MACH2Simulations@users.noreply.github.com>
2024-06-06 11:07:47 +0100

    Handle Args

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PrinterAddV2
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainProgressIndicator = new System.Windows.Forms.ProgressBar();
+            this.StatusText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // MainProgressIndicator
+            // 
+            this.MainProgressIndicator.Location = new System.Drawing.Point(13, 12);
+            this.MainProgressIndicator.Name = "MainProgressIndicator";
+            this.MainProgressIndicator.Size = new System.Drawing.Size(459, 23);
+            this.MainProgressIndicator.TabIndex = 0;
+            this.MainProgressIndicator.Click += new System.EventHandler(this.MainProgressIndicator_Click);
+            // 
+            // StatusText
+            // 
+            this.StatusText.Enabled = false;
+            this.StatusText.Location = new System.Drawing.Point(12, 49);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(457, 20);
+            this.StatusText.TabIndex = 1;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 81);
+            this.ControlBox = false;
+            this.Controls.Add(this.StatusText);
+            this.Controls.Add(this.MainProgressIndicator);
+            this.MaximumSize = new System.Drawing.Size(500, 120);
+            this.MinimumSize = new System.Drawing.Size(500, 120);
+            this.Name = "MainWindow";
+            this.Text = "Printer Adder V2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar MainProgressIndicator;
+        private System.Windows.Forms.TextBox StatusText;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrinterAddV2
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();
+            //StatusText.Text = args[3];
+            Handle_Args(args);
+
+        }
+
+        private void MainProgressIndicator_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public string Handle_Args(Array Args) {
+            string[] PrintersToAdd = { "Null" };
+            string Server = "Null";
+            int Tries = 0;
+
+            foreach (var arg in Args)
+            {
+                if (Convert.ToString(arg) == "/qn" || Convert.ToString(arg) == "/QN")
+                {
+                    this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+                    this.ShowInTaskbar = false;
+                }
+                else if (Convert.ToString(arg).Contains("/s") || Convert.ToString(arg).Contains("/S"))
+                {
+                    Server = Convert.ToString(arg).Remove(0, 3);
+                    StatusText.Text = Server;
+                }
+                else if (Convert.ToString(arg).Contains("/t") || Convert.ToString(arg).Contains("/T"))
+                {
+                    Tries = int.Parse((Convert.ToString(arg).Remove(0, 3)));
+                    Console.WriteLine(Tries);
+                }
+                else if (Convert.ToString(arg).Contains("/p") || Convert.ToString(arg).Contains("/P"))
+                {
+                    string Printers = Convert.ToString(arg).Remove(0, 3);
+                    PrintersToAdd = Printers.Split(' ');
+
+                }
+
+            }
+
+            Add_Printers(PrintersToAdd,Server,Tries);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            return "done";
+
+        
+        }
+
+        public string Add_Printers(string[] Args,string Server, int Tries = 5)
+        {
+            StatusText.Text = Server + " " + Args[1];
+
+
+            return "done";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
